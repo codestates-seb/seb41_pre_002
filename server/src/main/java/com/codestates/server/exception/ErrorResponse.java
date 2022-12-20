18e5e81db7c91d31f37df23c13bbbbcf8fd562a7
harin95 <harin4rang@gmail.com>
2022-12-21 01:01:28 +0900

    Feat: error response 추가

---
+++ server/src/main/java/com/codestates/server/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.codestates.server.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    private ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
+    }
+}
