da5751112ba769bcfc5f04fc561ce665c84df061
harin95 <harin4rang@gmail.com>
2022-12-20 15:35:25 +0900

    Feat: member entity 추가

---
+++ server/src/main/java/com/codestates/server/member/entity/Member.java
@@ -0,0 +1,12 @@
+package com.codestates.server.member.entity;
+
+import java.util.Date;
+
+public class Member {
+    Long memberId;
+    String email;
+    String memberName;
+    String memberPassword;
+    Date createdAt;
+    Date modifiedAt;
+}
