a19141c74244926a27e0021b70d277eaef4e824e
harin95 <harin4rang@gmail.com>
2022-12-20 17:27:47 +0900

    Rename: 파일명 변경

--- server/src/main/java/com/codestates/server/member/dto/MemberDto.java
+++ server/src/main/java/com/codestates/server/member/dto/MemberDto.java
@@ -3,7 +3,6 @@ package com.codestates.server.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
