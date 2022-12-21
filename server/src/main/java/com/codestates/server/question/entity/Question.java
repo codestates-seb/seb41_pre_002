fc6ae217755ade14c7761b3ca765d50e2de44d63
Ksiyeong <yuiop2767@naver.com>
2022-12-21 09:55:54 +0900

    Feat: Question 엔티티 List<Answer> 추가
    Question 엔티티에 Answer 과 1대N 연관관계 매핑 추가

--- server/src/main/java/com/codestates/server/question/entity/Question.java
+++ server/src/main/java/com/codestates/server/question/entity/Question.java
@@ -1,7 +1,7 @@
 package com.codestates.server.question.entity;
 
+import com.codestates.server.answer.entity.Answer;
 import com.codestates.server.audit.Auditable;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,7 +28,9 @@ public class Question extends Auditable {
 
 //Todo:    private Member member; // 다대일 연관관계 매핑 예
 
-//Todo:    private List<Answer> answers = new ArrayList<>(); // 일대다 연관관계 매핑 예정
+    @OneToMany(mappedBy = "question")
+    private List<Answer> answers = new ArrayList<>(); // 일대다 연관관계 매핑 예정
+
 
     // 연관 관계 매핑 관련 메서드
     public void addQuestionTag(QuestionTag questionTag) {
