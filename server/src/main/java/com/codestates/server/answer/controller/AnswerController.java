f15f31ad2e10220d3269e0c0b4a48baa508cce83
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-21 11:07:24 +0900

    feat : AnswerController Post기능 구현.

--- server/src/main/java/com/codestates/server/answer/controller/AnswerController.java
+++ server/src/main/java/com/codestates/server/answer/controller/AnswerController.java
@@ -1,23 +1,33 @@
 package com.codestates.server.answer.controller;
 
+import com.codestates.server.answer.dto.AnswerDto;
+import com.codestates.server.answer.entity.Answer;
 import com.codestates.server.answer.mapper.AnswerMapper;
 import com.codestates.server.answer.service.AnswerService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/answer")
+@RequestMapping
 @RequiredArgsConstructor
+@Validated
 public class AnswerController {
 
     private final AnswerService answerService;
     private final AnswerMapper mapper;
 
-    @PostMapping
-    public ResponseEntity postAnswer() {
-        return null;
+    @PostMapping("/questions/{question-id}/answers")
+    public ResponseEntity postAnswer( @PathVariable("question-id") long questionId,
+                                     @Valid  @RequestBody AnswerDto.Post requestBody) {
+        Answer answer = mapper.AnswerPostDtoToAnswer(requestBody);
+        Answer response = answerService.createAnswer(answer, questionId);
+        return new ResponseEntity<>(mapper.AnswerToAnswerResponseDto(response), HttpStatus.CREATED);
     }
     @GetMapping
     public ResponseEntity getAnswer() {
