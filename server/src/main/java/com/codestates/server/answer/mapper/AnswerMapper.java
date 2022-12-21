cc7e348dbdf5d4bf35d8b3832627c9edde2fc1dc
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-21 11:08:08 +0900

    feat : AnswerMapper Response반환 부분 구현.

--- server/src/main/java/com/codestates/server/answer/mapper/AnswerMapper.java
+++ server/src/main/java/com/codestates/server/answer/mapper/AnswerMapper.java
@@ -5,12 +5,31 @@ import com.codestates.server.answer.entity.Answer;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring",unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface AnswerMapper {
     Answer AnswerPostDtoToAnswer(AnswerDto.Post requestBody);
     Answer AnswerPatchDtoToAnswer(AnswerDto.Patch requestBody);
-    AnswerDto.Response AnswerToAnswerResponseDto(Answer answer);
+
     List<AnswerDto.Response> AnswersToAnswerResponseDtos(List<Answer> answers);
+
+    default AnswerDto.Response AnswerToAnswerResponseDto(Answer answer) {
+        if (answer == null) {
+            return null;
+        } else {
+            String content = null;
+            LocalDateTime createdAt = null;
+            LocalDateTime modifiedAt = null;
+            String memberName = null;
+            content = answer.getContent();
+            createdAt = answer.getCreatedAt();
+            modifiedAt = answer.getModifiedAt();
+            memberName = answer.getMember().getMemberName();
+            AnswerDto.Response response = new AnswerDto.Response((String) memberName, content, createdAt, modifiedAt);
+            return response;
+        }
+    }
 }
+
