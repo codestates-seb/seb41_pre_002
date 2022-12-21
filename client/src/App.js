5ee36553c3bad51656c85e063d1f9f02b45f3102
hyob12 <gyqud5981@gmail.com>
2022-12-21 16:17:56 +0900

    feat: 헤더추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import Header from './component/header.js';
-import Nav from './component/nav.js';
-import Footer from './component/footer.js';
-
-
 
 function App() {
   return (
-    <>
-    <Header />
-    <Nav />
-    <Footer />
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
