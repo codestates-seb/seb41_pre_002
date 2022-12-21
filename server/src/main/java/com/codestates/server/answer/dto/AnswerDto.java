feb665cb496e0d22184f1bc9b8f3d0f5ddf74121
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-21 15:10:01 +0900

    Feat : AnswerDto와 Mapper 코드 수정.

--- server/src/main/java/com/codestates/server/answer/dto/AnswerDto.java
+++ server/src/main/java/com/codestates/server/answer/dto/AnswerDto.java
@@ -1,6 +1,6 @@
 package com.codestates.server.answer.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
@@ -9,17 +9,25 @@ import java.time.LocalDateTime;
 public class AnswerDto {
     @Getter
     public static class Post {
+        private Long memberId;
+        private Long questionId;
+        private String memberName;
         @NotBlank
         private String content;
     }
     @Getter
     public static class Patch {
+        private Long memberId;
+        private Long questionId;
+        private String memberName;
         @NotBlank
         private String content;
     }
     @Getter
-    @AllArgsConstructor
+    @Builder
     public static class Response {
+        private Long memberId;
+        private Long questionId;
         private String memberName;
         private String content;
         private LocalDateTime createdAt;
