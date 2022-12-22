b5ca056bb4e0164f5232ccfb1de1fb3ac7c2f70f
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-22 14:20:00 +0900

    feat : comment dto 필드 추가.

--- server/src/main/java/com/codestates/server/comment/dto/CommentDto.java
+++ server/src/main/java/com/codestates/server/comment/dto/CommentDto.java
@@ -9,16 +9,22 @@ public class CommentDto {
 
     @Getter
     public static class Post {
+        private Long memberId;
+        private Long questionId;
         private String content;
     }
     @Getter
     public static class Patch {
-        private String comment;
+        private Long memberId;
+        private Long questionId;
+        private String content;
     }
     @Getter
     @AllArgsConstructor
     public static class Response {
-        private String comment;
+        private Long questionId;
+        private Long memberId;
+        private String content;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
     }
