8e0c6eb2730d86c81acd9c7970b358fa9ee20d84
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-22 18:01:02 +0900

    Refactor : CommentDto 필드 추가

--- server/src/main/java/com/codestates/server/comment/dto/CommentDto.java
+++ server/src/main/java/com/codestates/server/comment/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package com.codestates.server.comment.dto;
 
+import com.codestates.server.audit.AuditableResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,21 +12,25 @@ public class CommentDto {
     public static class Post {
         private Long memberId;
         private Long questionId;
+        private Long answerId;
         private String content;
     }
     @Getter
     public static class Patch {
         private Long memberId;
         private Long questionId;
+        private Long answerId;
         private String content;
     }
     @Getter
     @AllArgsConstructor
     public static class Response {
         private Long questionId;
+        private Long answerId;
         private Long memberId;
         private String content;
-        private LocalDateTime createdAt;
-        private LocalDateTime modifiedAt;
+        private AuditableResponseDto auditableResponseDto;
+        /*private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;*/
     }
 }
