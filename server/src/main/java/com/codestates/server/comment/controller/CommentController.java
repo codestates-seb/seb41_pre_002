090d5cffe6e3d8d4b03c47bff3fbad3b40d9cc4b
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-22 14:18:51 +0900

    feat : comment controller post, patch, delete 구현 초안.

--- server/src/main/java/com/codestates/server/comment/controller/CommentController.java
+++ server/src/main/java/com/codestates/server/comment/controller/CommentController.java
@@ -1,34 +1,64 @@
 package com.codestates.server.comment.controller;
 
+import com.codestates.server.answer.repository.AnswerRepository;
+import com.codestates.server.comment.dto.CommentDto;
+import com.codestates.server.comment.entity.Comment;
 import com.codestates.server.comment.mapper.CommentMapper;
 import com.codestates.server.comment.service.CommentService;
+import com.codestates.server.dto.SingleResponseDto;
+import com.sun.net.httpserver.HttpServer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.text.ParseException;
+
 @RestController
-@RequestMapping("/comment")
+@RequestMapping
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final CommentService CommentService;
+    private final CommentService commentService;
     private final CommentMapper mapper;
+    private final AnswerRepository answerRepository;
 
-    @PostMapping
-    public ResponseEntity postComment() {
-        return null;
+    @PostMapping("/questions/{question-id}/comments")
+    public ResponseEntity postQuestionComment(@PathVariable("question-id") Long questionId,
+                                       @Valid @RequestBody CommentDto.Post requestBody) {
+
+        Comment comment = mapper.CommentPostDtoToComment(requestBody);
+        Comment response = commentService.postQuestionComment(questionId, comment);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.CommentToCommentResponseDto(response)), HttpStatus.CREATED);
     }
-    @GetMapping
-    public ResponseEntity getComment() {
-        return null;
+
+    @PostMapping("/answers/{answers-id}/comments")
+    public ResponseEntity postAnswerComment( @PathVariable("answer-id") Long answerId,
+                                       @Valid @RequestBody CommentDto.Post requestBody) {
+
+        Comment comment = mapper.CommentPostDtoToComment(requestBody);
+        Comment response = commentService.postAnswerComment(answerId, comment);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.CommentToCommentResponseDto(response)), HttpStatus.CREATED);
+
     }
-    @PatchMapping
-    public ResponseEntity patchComment() {
-        return null;
+    @PatchMapping("/comments/{comment-id}}")
+    public ResponseEntity patchComment(@RequestParam("comment-id") Long commentId,
+                                       @RequestBody CommentDto.Patch requestBody,
+                                       HttpServletRequest request) {
+
+        Comment comment = mapper.CommentPatchDtoToComment(requestBody);
+        Comment response = commentService.updateComment(commentId, comment);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.CommentToCommentResponseDto(response)), HttpStatus.OK);
     }
-    @DeleteMapping
-    public ResponseEntity deleteComment() {
-        return null;
+    @DeleteMapping("/comments/{comment-id}")
+    public ResponseEntity deleteComment(@RequestParam("comment-id") Long commentId,
+                                        HttpServletRequest request) {
+
+        commentService.deleteComment(commentId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
 
