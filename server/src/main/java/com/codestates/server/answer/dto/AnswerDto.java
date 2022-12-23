450da3b2d1f6d257f8a0f45a0f1fc56b94f1b22a
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-23 15:22:03 +0900

    feat : AnswerDto 코드 수정

--- server/src/main/java/com/codestates/server/answer/dto/AnswerDto.java
+++ server/src/main/java/com/codestates/server/answer/dto/AnswerDto.java
@@ -1,12 +1,13 @@
 package com.codestates.server.answer.dto;
 
 import com.codestates.server.audit.AuditableResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
-
+@AllArgsConstructor // tset 를 위해 추가.
 public class AnswerDto {
     @Getter
     public static class Post {
