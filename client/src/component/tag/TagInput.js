5955c8ee51c73186cb59943a9e3216ddbfa96483
kangseong-sim <stylesha34@gmail.com>
2022-12-23 12:28:55 +0900

    feat : tag input 완료

--- client/src/component/tag/TagInput.js
+++ client/src/component/tag/TagInput.js
@@ -1,45 +1,32 @@
 import React from "react";
-import styled from "styled-components";
 
-const InputDiv = styled.div`
-  /* display: flex; */
-
-  .tags-input-group {
-    /* position: absolute; */
-    display: flex;
-    
-  }
-`
+// 태그 검색 
 
 const Taginput = () => {
   return (
-    <InputDiv>
-      <form id="search" class="s-topbar--searchbar" autocomplete="off">
-        <div class="tags-input-group">
-          <input
-            type="text"
-            placeholder="Fillter by tag name"
-            value=""
-            autocomplete="off"
-            class="s-input s-input__search"
-            
-          />
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 1024 1024"
-            color="rgb(159, 166, 173)"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-            // style="color: rgb(159, 166, 173);"
-          >
-            <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-          </svg>
-        </div>
-      </form>
-    </InputDiv>
+    <>
+      <div class="flex--item ps-relative mb12">
+        <input
+          id="tagfilter"
+          class="s-input s-input__search h100 js-tag-filter"
+          autocomplete="off"
+          name="tagfilter"
+          type="text"
+          maxlength="35"
+          placeholder="Filter by tag name"
+          autofocus=""
+        ></input>
+        <svg
+          aria-hidden="true"
+          class="s-input-icon s-input-icon__search svg-icon iconSearch"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+        >
+          <path d="m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z"></path>
+        </svg>
+      </div>
+    </>
   );
 };
 
