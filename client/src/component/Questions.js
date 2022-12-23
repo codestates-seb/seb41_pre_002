3082282f718909133a74af84ea457957418bce0e
kangseong-sim <stylesha34@gmail.com>
2022-12-23 12:56:30 +0900

    Feat: question 수정

--- client/src/component/Questions.js
+++ client/src/component/Questions.js
@@ -1,39 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 
-//dkdklf
-const ContentDiv = styled.div`
-`;
+//
+const ContentDiv = styled.div``;
 
 const ContentHeader = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 24px 16px;
 
   h1 {
-    font-size: 13px;
+    font-size: 27px;
     margin: 0px 0px 27px;
+    color: black;
+    font-weight: 400;
   }
 `;
 
 const AskButton = styled.button`
   width: 105px;
   height: 40px;
+  font-size: 13px;
   background-color: #0a95ff;
   padding: 10px;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  box-sizing: border-box;
+  box-shadow: rgba(255, 255, 255, 0.4) 0px 1px;
 `;
 
 const ButtonBox = styled.div`
   display: flex;
   justify-content: flex-end;
   margin: 0px 0px 16px;
-  
-  button{
+
+  button {
     background-color: #ffffff;
     color: #6a737c;
     margin: 0px -1px -1px 0px;
     padding: 10px;
     border: 1px solid;
   }
-  
 `;
 
 const QuestionSummary = styled.div`
@@ -41,9 +49,8 @@ const QuestionSummary = styled.div`
   display: flex;
   border-bottom: 1px solid #e3e6e8;
   padding: 16px;
-  width: 600px;
-  height: 105px;
-  background-color: #fdf7e2;
+  width: 750px;
+  height: 120px;
   /* color: #6A737C; */
   /* &.question-mini-list:last-child {
     border-bottom-width: 0;
@@ -53,22 +60,25 @@ const QuestionSummary = styled.div`
   } */
 `;
 
-
 const SummaryStatus = styled.div`
   margin: 0px 16px 4px 0px;
   color: #6a737c;
 `;
 
-const SummaryContent = styled.div``;
+const SummaryContent = styled.div`
+  color: #6a737c;
+`;
 
 const Questions = () => {
   return (
     <>
-      <ContentDiv> 
+      <ContentDiv>
+        {/*question 상단 */}
         <ContentHeader>
           <h1 className="QuestionHeader">Top Questions</h1>
           <AskButton>Ask Question</AskButton>
         </ContentHeader>
+        {/* question 관련 버튼 */}
         <ButtonBox>
           <button>Interesting</button>
           <button>Bountied</button>
@@ -76,6 +86,7 @@ const Questions = () => {
           <button>Week</button>
           <button>Month</button>
         </ButtonBox>
+        {/* question을 요약해서 보여주는 하나의 박스 */}
         <QuestionSummary>
           <SummaryStatus>status</SummaryStatus>
           <SummaryContent>CONTENTS</SummaryContent>
