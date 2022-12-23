0790e0c7c1b0f208f4b40bb551376a118ecf7a18
Ksiyeong <yuiop2767@naver.com>
2022-12-23 11:38:56 +0900

    Refactor: Vote 엔티티 수정
    Vote 엔티티 Member 및 각 N대1 연관관계 매핑 추가

--- server/src/main/java/com/codestates/server/vote/entity/AnswerVote.java
+++ server/src/main/java/com/codestates/server/vote/entity/AnswerVote.java
@@ -1,11 +1,11 @@
 package com.codestates.server.vote.entity;
 
+import com.codestates.server.answer.entity.Answer;
+import com.codestates.server.member.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -13,4 +13,17 @@ public class AnswerVote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long voteId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "ANSWER_ID")
+    private Answer answer;
+
+    public AnswerVote(Member member, Answer answer) {
+        this.member = member;
+        this.answer = answer;
+    }
 }
--- server/src/main/java/com/codestates/server/vote/entity/QuestionVote.java
+++ server/src/main/java/com/codestates/server/vote/entity/QuestionVote.java
@@ -1,11 +1,10 @@
 package com.codestates.server.vote.entity;
 
+import com.codestates.server.member.entity.Member;
+import com.codestates.server.question.entity.Question;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -13,4 +12,17 @@ public class QuestionVote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long voteId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    public QuestionVote(Member member, Question question) {
+        this.member = member;
+        this.question = question;
+    }
 }
