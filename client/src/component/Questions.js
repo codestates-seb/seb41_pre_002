a91718a349226c8b2d461fb17ffcc60764229511
kangseong-sim <stylesha34@gmail.com>
2022-12-26 17:19:31 +0900

    feat: question 버튼 수정

--- client/src/component/Questions.js
+++ client/src/component/Questions.js
@@ -35,13 +35,27 @@ const ButtonBox = styled.div`
   display: flex;
   justify-content: flex-end;
   margin: 0px 0px 16px;
+    border-color: var(--_bu-outlined-bc-selected);
+    border-radius: 4px;
 
-  button {
-    background-color: #ffffff;
-    color: #6a737c;
-    margin: 0px -1px -1px 0px;
-    padding: 10px;
-    border: 1px solid;
+    .Btn {
+    /* display: flex; */
+    height: 40px;
+    width: 80px;
+    background-color: var(--_bu-outlined-bg);
+    border-color: var(--_bu-outlined-bc-selected);
+    color: black;
+    font-size: 13px;
+    font-weight: 400;
+    border: 1px solid rgb(214, 217, 220);
+    &:active {
+      border-color: rgb(227, 230, 232);
+      background-color: rgb(227, 230, 232);
+      color: rgb(59, 64, 60);
+    }
+    &:hover {
+      background-color: rgb(242, 244, 245);
+    }
   }
 `;
 
@@ -83,11 +97,11 @@ const Questions = () => {
         </ContentHeader>
         {/* question 관련 버튼 */}
         <ButtonBox>
-          <button>Interesting</button>
-          <button>Bountied</button>
-          <button>Hot</button> {/*dkjfkldjflk*/}
-          <button>Week</button>
-          <button>Month</button>
+          <button className="Btn">Interesting</button>
+          <button className="Btn">Bountied</button>
+          <button className="Btn">Hot</button> 
+          <button className="Btn">Week</button>
+          <button className="Btn">Month</button>
         </ButtonBox>
         {/* question을 요약해서 보여주는 하나의 박스 */}
         <QuestionSummary>
