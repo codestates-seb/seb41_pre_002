d71983006e61d4b085e85348c7567018250416e0
Ksiyeong <yuiop2767@naver.com>
2022-12-26 23:58:40 +0900

    Feat: 질문검색 구현
    QuestionRepository - 질문검색메서드 추가

--- server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
+++ server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.codestates.server.question.repository;
 
 import com.codestates.server.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Page<Question> findAllByTitleContainsOrContentContains(String title, String content, Pageable pageable);
+
 }
