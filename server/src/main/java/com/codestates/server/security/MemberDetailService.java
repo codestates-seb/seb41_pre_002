34631cf1279e4f81ad2bbbb868deb784d5accb19
harin95 <harin4rang@gmail.com>
2022-12-28 04:25:34 +0900

    Refactor: 파일명 변경

--- server/src/main/java/com/codestates/server/security/MemberDetailService.java
+++ server/src/main/java/com/codestates/server/security/MemberDetailService.java
@@ -7,25 +7,27 @@ import com.codestates.server.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Collection;
 import java.util.Optional;
 
 @RequiredArgsConstructor
-public class UserDetailService {
+public class MemberDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
-    private final UserAuthorityUtils userAuthorityUtils;
+    private final CustomAuthorityUtils userAuthorityUtils;
 
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Member> optionalMember = memberRepository.findByEmail(username);
         Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
 
-        return new HelloUserDetails(findMember);
+        return new MemberDetails(findMember);
     }
 
-    private final class HelloUserDetails extends Member implements UserDetails {
-        HelloUserDetails(Member member) {
+    private final class MemberDetails extends Member implements UserDetails {
+        MemberDetails(Member member) {
             setMemberId(member.getMemberId());
             setMemberName(member.getMemberName());
             setEmail(member.getEmail());
@@ -45,27 +47,27 @@ public class UserDetailService {
 
         @Override
         public String getUsername() {
-            return null;
+            return this.getEmail();
         }
 
         @Override
         public boolean isAccountNonExpired() {
-            return false;
+            return true;
         }
 
         @Override
         public boolean isAccountNonLocked() {
-            return false;
+            return true;
         }
 
         @Override
         public boolean isCredentialsNonExpired() {
-            return false;
+            return true;
         }
 
         @Override
         public boolean isEnabled() {
-            return false;
+            return true;
         }
     }
 }
