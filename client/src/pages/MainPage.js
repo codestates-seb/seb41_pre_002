b7668d51642d978ea333b1270225cfd3fbb1fb90
kangseong-sim <stylesha34@gmail.com>
2022-12-27 17:44:42 +0900

    Refactor: main axios

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../component/header";
 import Footer from "../component/footer";
 import Nav from "../component/nav";
 import Questions from "../component/question/Questions";
+import axios from "axios";
 
 const Maindiv = styled.div`
   width: 100vw;
@@ -13,12 +14,31 @@ const Maindiv = styled.div`
 `;
 
 function MainPage() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/questions", {
+        
+      })
+      .then((response) => {
+        console.log(response.data.data); //정상 통신 후 응답된 메시지 출력
+      })
+      .catch((error) => {
+        console.log(error);
+        //오류발생시 실행
+      })
+      .then((response) => {
+        setQuestions(response.data.data); //정상 통신 후 응답된 메시지 출력
+      });
+  }, []);
+
   return (
     <>
       <Header />
       <Maindiv>
         <Nav />
-        <Questions />
+        <Questions questions={questions} color="black"/>
       </Maindiv>
       <Footer />
     </>
