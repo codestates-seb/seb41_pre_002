61a0cf0889953ffd80623a1d7bbb6f510779ced0
harin95 <harin4rang@gmail.com>
2022-12-27 14:14:44 +0900

    Feat: UserDetailService 추가

---
+++ server/src/main/java/com/codestates/server/security/UserDetailService.java
@@ -0,0 +1,71 @@
+package com.codestates.server.security;
+
+import com.codestates.server.exception.BusinessLogicException;
+import com.codestates.server.exception.ExceptionCode;
+import com.codestates.server.member.entity.Member;
+import com.codestates.server.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class UserDetailService {
+    private final MemberRepository memberRepository;
+    private final UserAuthorityUtils userAuthorityUtils;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new HelloUserDetails(findMember);
+    }
+
+    private final class HelloUserDetails extends Member implements UserDetails {
+        HelloUserDetails(Member member) {
+            setMemberId(member.getMemberId());
+            setMemberName(member.getMemberName());
+            setEmail(member.getEmail());
+            setMemberPassword(member.getMemberPassword());
+            setRoles(member.getRoles());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return userAuthorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getPassword() {
+            return null;
+        }
+
+        @Override
+        public String getUsername() {
+            return null;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return false;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+    }
+}
