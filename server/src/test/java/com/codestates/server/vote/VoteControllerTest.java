037369b7def4c3056862bd40547c75a563647f53
Ksiyeong <yuiop2767@naver.com>
2022-12-28 12:03:07 +0900

    Test: Test 케이스 작성 완료

--- server/src/test/java/com/codestates/server/vote/VoteControllerTest.java
+++ server/src/test/java/com/codestates/server/vote/VoteControllerTest.java
@@ -1,5 +1,6 @@
 package com.codestates.server.vote;
 
+import com.codestates.server.vote.controller.VoteController;
 import com.codestates.server.vote.dto.VoteRequestDto;
 import com.codestates.server.vote.service.VoteService;
 import com.google.gson.Gson;
@@ -9,10 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -20,20 +17,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@WebMvcTest(VoteControllerTest.class)
+
+@WebMvcTest(VoteController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureRestDocs
 public class VoteControllerTest {
@@ -57,7 +54,7 @@ public class VoteControllerTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                post("/votes/questions/{request-id}", 1L)
+                post("/votes/questions/{question-id}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content)
         );
@@ -68,7 +65,44 @@ public class VoteControllerTest {
                 .andDo(document(
                         "post-questionVote",
                         preprocessRequest(prettyPrint()),
-                        pathParameters(parameterWithName("request-id").description("추천 대상이 되는 질문의 식별자")),
+                        pathParameters(parameterWithName("question-id").description("추천 대상이 되는 질문의 식별자")),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("score").type(JsonFieldType.NUMBER).description("요청 점수 : 추천 = 1, 비추천 = -1")
+                                )
+                        )
+                ))
+                .andReturn();
+
+        System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
+    }
+
+    @Test
+    void postAnswerVote() throws Exception {
+        //given
+        VoteRequestDto post = new VoteRequestDto();
+        post.setMemberId(1L);
+        post.setScore(1);
+
+        doNothing().when(voteService).doVote(Mockito.anyChar(), Mockito.anyInt(), Mockito.anyLong(), Mockito.anyLong());
+
+        String content = gson.toJson(post);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/votes/answers/{answer-id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        //then
+        MvcResult result = actions
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "post-answerVote",
+                        preprocessRequest(prettyPrint()),
+                        pathParameters(parameterWithName("answer-id").description("추천 대상이 되는 답변의 식별자")),
                         requestFields(
                                 List.of(
                                         fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
