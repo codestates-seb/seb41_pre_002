da9f7714bbf4981ae34c0e383933b6c273dba9a1
Ksiyeong <yuiop2767@naver.com>
2022-12-28 14:01:25 +0900

    Feat: 질문 전체 조회 필터기능 구현
    QuestionRepository - 필터 관련 메서드 추가
    QuestionService - 필터 메서드 적용

--- server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
+++ server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
@@ -8,4 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findAllByTitleContainsOrContentContains(String title, String content, Pageable pageable);
 
+    Page<Question> findAllByAnswerCountIs(Integer answerCount, Pageable pageable);
+
+    Page<Question> findAllByAnswerCountGreaterThan(Integer answerCount, Pageable pageable); // 질문이 존재하는 경우
+
+    Page<Question> findAllByAnswerCountIsAndTitleContainsOrContentContains(Integer answerCount, String title, String content, Pageable pageable);
+
+    Page<Question> findAllByAnswerCountGreaterThanAndTitleContainsOrContentContains(Integer answerCount, String title, String content, Pageable pageable);
 }
