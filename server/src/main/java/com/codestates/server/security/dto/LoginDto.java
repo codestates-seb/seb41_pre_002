c6aeb3ae59f99163bc9cd5a43effadecc8292916
harin95 <harin4rang@gmail.com>
2022-12-29 00:02:07 +0900

    Feat: 어노테이션 추가

--- server/src/main/java/com/codestates/server/security/dto/LoginDto.java
+++ server/src/main/java/com/codestates/server/security/dto/LoginDto.java
@@ -1,8 +1,10 @@
 package com.codestates.server.security.dto;
 
+import lombok.Data;
 import lombok.Getter;
 
 @Getter
+@Data
 public class LoginDto {
     private String username;
     private String password;
