c4d130561311feadc89671c40e6c8d7475b878bf
harin95 <harin4rang@gmail.com>
2022-12-30 06:05:54 +0900

    Feat: api 접근 권한 설정

--- server/src/main/java/com/codestates/server/security/SecurityConfig.java
+++ server/src/main/java/com/codestates/server/security/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -28,17 +30,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .headers().frameOptions().disable()
+                .headers().frameOptions().sameOrigin()
+//                .headers().frameOptions().disable()   //h2-console 사용하려면 위에 것 주석처리하고 이것 사용
                 .and()
-                .httpBasic().disable()
                 .csrf().disable()
+                .cors(withDefaults())
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .formLogin().disable()
+                .httpBasic().disable()
                 .authorizeRequests()
-                .antMatchers("/signUp", "/login").permitAll()
+                .antMatchers("/signUp", "/login", "/home").permitAll()
                 .antMatchers("/auth/user").hasRole("USER")
-                .antMatchers("/auth/admin").hasRole("ADMIN")
-                .anyRequest().permitAll()
+                .antMatchers("/auth/admin", "/members").hasRole("ADMIN")    //관리자 권한만 이곳에 설정
+                .anyRequest().hasAnyRole("USER", "ADMIN")   //나머지는 관리자, 유저 모두 접근 가능 & 권한 없으면 접근 불가
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
