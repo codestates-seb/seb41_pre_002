4f2b60199122e1c972812b8f591043a0a2edd832
Ksiyeong <yuiop2767@naver.com>
2022-12-29 11:44:14 +0900

    Feat: conflict해결을 위한 수정

--- server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
+++ server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
@@ -12,4 +12,13 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     @Query("select q from Question q join fetch q.member join fetch q.answers a")
     Optional<Question> findByQuestionId(Long questionId);
+
+
+    Page<Question> findAllByAnswerCountIs(Integer answerCount, Pageable pageable);
+
+    Page<Question> findAllByAnswerCountGreaterThan(Integer answerCount, Pageable pageable); // 질문이 존재하는 경우
+
+    Page<Question> findAllByAnswerCountIsAndTitleContainsOrContentContains(Integer answerCount, String title, String content, Pageable pageable);
+
+    Page<Question> findAllByAnswerCountGreaterThanAndTitleContainsOrContentContains(Integer answerCount, String title, String content, Pageable pageable);
 }
