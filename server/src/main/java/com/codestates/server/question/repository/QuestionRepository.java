df3c068771ce84774a470ea565e225af50b382cc
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-29 11:08:55 +0900

    feat : 페치 조인 검색기능 구현

--- server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
+++ server/src/main/java/com/codestates/server/question/repository/QuestionRepository.java
@@ -4,8 +4,12 @@ import com.codestates.server.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findAllByTitleContainsOrContentContains(String title, String content, Pageable pageable);
 
+    @Query("select q from Question q join fetch q.member join fetch q.answers a")
+    Optional<Question> findByQuestionId(Long questionId);
 }
