cfacd8d93820da002e1109d9473c5d16e7b19ae3
hyob12 <gyqud5981@gmail.com>
2022-12-31 01:09:24 +0900

    feat: 로그인  상태 & 로컬스토리지

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import QuesetionRead from "./pages/QuesetionRead";
-import QuestionWrite from "./pages/QuestionWrite";
-import MyPage from "./pages/MyPage";
-import LoginPage from "./pages/LoginPage";
-// import LogoutPage from "./pages/LogoutPage";
-import SignInPage from "./pages/SignInPage";
-import TagPage from "./pages/TagPage";
-import UserPage from "./pages/UserPage";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import QuesetionRead from './pages/QuesetionRead';
+import QuestionWrite from './pages/QuestionWrite';
+import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage';
+import LogoutPage from './pages/LogoutPage';
+import SignInPage from './pages/SignInPage';
+import TagPage from './pages/TagPage';
+import UserPage from './pages/UserPage';
+import Header from './component/header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [questions, setQuestions] = useState("");
+  const [questions, setQuestions] = useState('');
   const [number, setnumber] = useState(1);
+  const [isLogin, setIsLogin] = useState(() => JSON.parse(window.localStorage.getItem('isLogin')));
+
+  useEffect(() => {
+    window.localStorage.setItem('isLogin', JSON.stringify(isLogin));
+  }, [isLogin]);
 
   useEffect(() => {
     axios
       .get(`/questions/${number}`, {})
       .then((response) => {
-        console.log(response.data.data.questionResponseDto.title); //정상 통신 후 응답된 메시지 출력
+        // console.log(response.data.data.questionResponseDto.title); //정상 통신 후 응답된 메시지 출력
         setQuestions(response.data.data.questionResponseDto); //정상 통신 후 응답된 메시지 출력
       })
       .catch((error) => {
@@ -32,16 +38,14 @@ function App() {
     <>
       <BrowserRouter>
         <div>
+          <Header isLogin={isLogin} />
           <Routes>
             <Route path="/" element={<MainPage questions={questions} />} />
-            <Route
-              path="/QuesetionRead"
-              element={<QuesetionRead questions={questions} />}
-            />
+            <Route path="/QuesetionRead" element={<QuesetionRead questions={questions} />} />
             <Route path="/QuestionWrite" element={<QuestionWrite />} />
             <Route path="/MyPage" element={<MyPage />} />
-            <Route path="/LoginPage" element={<LoginPage />} />
-            {/* <Route path="/LogoutPage" element={<LogoutPage />} /> */}
+            <Route path="/LoginPage" element={<LoginPage setIsLogin={setIsLogin} />} />
+            <Route path="/LogoutPage" element={<LogoutPage setIsLogin={setIsLogin} />} />
             <Route path="/SignInPage" element={<SignInPage />} />
             <Route path="/TagPage" element={<TagPage />} />
             <Route path="/UserPage" element={<UserPage />} />
