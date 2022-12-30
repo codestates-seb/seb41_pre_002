43ac7f61c18b9413082bfd8f449c5b4d60853c42
hyob12 <gyqud5981@gmail.com>
2022-12-31 01:09:24 +0900

    feat: 로그인  상태 & 로컬스토리지

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DetailsQustion from "./component/DetailsQustion";
-import QuestionWrite from "./pages/QuestionWrite";
-import Profile from "./component/Profile";
-import LoginPage from "./pages/LoginPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailsQustion from './component/DetailsQustion';
+import QuestionWrite from './pages/QuestionWrite';
+import Profile from './component/Profile';
+import LoginPage from './pages/LoginPage';
 // import LogoutPage from "./pages/LogoutPage";
-import SignInPage from "./pages/SignInPage";
-import Tag from "./component/tag/Tag.js";
-import Users from "./component/user/Users";
-import QuestionTagPage from "./pages/QuestionTagPage";
-import { useState, useEffect } from "react";
-import styled from "styled-components";
-import Header from "./component/header";
-import Questions from "./component/question/Questions";
-import Footer from "./component/footer";
-import Nav from "./component/nav";
-import axios from "axios";
+import SignInPage from './pages/SignInPage';
+import Tag from './component/tag/Tag.js';
+import Users from './component/user/Users';
+import QuestionTagPage from './pages/QuestionTagPage';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Header from './component/header';
+import Questions from './component/question/Questions';
+import Footer from './component/footer';
+import Nav from './component/nav';
+import axios from 'axios';
 
 const Appdiv = styled.div`
   width: 100vw;
@@ -25,14 +25,20 @@ const Appdiv = styled.div`
 `;
 
 function App() {
-  let [questionData, setQuestionData] = useState([]);
+  const [questions, setQuestions] = useState('');
+  const [number, setnumber] = useState(1);
+  const [isLogin, setIsLogin] = useState(() => JSON.parse(window.localStorage.getItem('isLogin')));
+
+  useEffect(() => {
+    window.localStorage.setItem('isLogin', JSON.stringify(isLogin));
+  }, [isLogin]);
 
   useEffect(() => {
     axios
       .get(`/questions`, {})
       .then((response) => {
-        setQuestionData(response.data);
-        console.log(response.data); // data& pageinfo
+        // console.log(response.data.data.questionResponseDto.title); //정상 통신 후 응답된 메시지 출력
+        setQuestions(response.data.data.questionResponseDto); //정상 통신 후 응답된 메시지 출력
       })
       .catch((error) => {
         console.log(error);
@@ -42,19 +48,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Appdiv>
-          <Nav />
+          <Header isLogin={isLogin} />
           <Routes>
-            <Route
-              path="/"
-              element={<Questions questionData={questionData} />}
-            />
-            <Route path="/DetailsQustion" element={<DetailsQustion />} />
+            <Route path="/" element={<MainPage questions={questions} />} />
+            <Route path="/QuesetionRead" element={<QuesetionRead questions={questions} />} />
             <Route path="/QuestionWrite" element={<QuestionWrite />} />
-            <Route path="/Profile" element={<Profile />} />
-            <Route path="/LoginPage" element={<LoginPage />} />
-            {/* <Route path="/LogoutPage" element={<LogoutPage />} /> */}
+            <Route path="/MyPage" element={<MyPage />} />
+            <Route path="/LoginPage" element={<LoginPage setIsLogin={setIsLogin} />} />
+            <Route path="/LogoutPage" element={<LogoutPage setIsLogin={setIsLogin} />} />
             <Route path="/SignInPage" element={<SignInPage />} />
             <Route path="/Tag" element={<Tag />} />
             <Route path="/QuestionTagPage" element={<QuestionTagPage />} />
