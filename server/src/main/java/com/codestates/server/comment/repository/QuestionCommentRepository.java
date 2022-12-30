29145ff63184683062f48e0256f522d8611bb49a
harin95 <76214729+harin95@users.noreply.github.com>
2022-12-30 12:51:53 +0900

    Revert "Server member jwt"

--- server/src/main/java/com/codestates/server/comment/repository/QuestionCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.server.comment.repository;
-
-import com.codestates.server.comment.entity.QuestionComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionCommentRepository extends JpaRepository<QuestionComment,Long> {
-}
