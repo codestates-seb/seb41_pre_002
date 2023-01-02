1c6833812803f821e6648a1572775df2219d99f6
samsamgo <jku2659@gmail.com>
2023-01-02 14:45:59 +0900

    post Detailquestions

--- client/src/component/DetailsQustion.js
+++ client/src/component/DetailsQustion.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import useForceUpdate from 'use-force-update';
-import { useSelector } from 'react-redux';
-import '../assets/fonts.css';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
+import React from "react";
+import styled from "styled-components";
+import useForceUpdate from "use-force-update";
+import { useSelector } from "react-redux";
+import "../assets/fonts.css";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 const PageDiv = styled.div`
   display: flex;
@@ -23,7 +23,8 @@ const PageHeader = styled.header`
 const Headernamediv = styled.div`
   border: border-box;
   color: rgb(59, 64, 69);
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: left;
   font-size: 30px;
   height: 937px;
@@ -38,7 +39,8 @@ const Askbuttondiv = styled.div`
 
 const Importmationdiv = styled.div`
   color: #6a737c;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: left;
   font-size: 12px;
 `;
@@ -73,7 +75,8 @@ const Qustioncontentdiv = styled.div`
   width: 690px;
   padding: 15px 30px 3px 30px;
   background-color: white;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: left;
   font-size: 15px;
   height: auto;
@@ -82,7 +85,8 @@ const Qustioncontentdiv = styled.div`
 `;
 const Codediv = styled.div`
   background-color: #f6f6f6;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: left;
   font-size: 15px;
   height: 937px;
@@ -100,7 +104,8 @@ const Tagboxdiv = styled.div`
 const Tagsdiv = styled.div`
   background-color: #e1ecf4;
   color: #39739d;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: center;
   font-size: 10px;
   border-radius: 5px;
@@ -111,7 +116,8 @@ const Tagsdiv = styled.div`
 const BolgAndReateddiv = styled.div`
   background-color: white;
   width: 350px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-align: left;
   font-size: 19px;
   display: flex;
@@ -123,7 +129,8 @@ const Relateddiv = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: 30px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   font-size: 13px;
   text-align: left;
   color: black;
@@ -137,7 +144,7 @@ const Relatedboxdiv = styled.div`
   width: 30px;
   height: 20px;
   margin: 0px 10px 0px 0px;
-  background-color: ${(props) => (props.color ? props.color : 'white')};
+  background-color: ${(props) => (props.color ? props.color : "white")};
   text-align: center;
   font-size: 10px;
   border-radius: 5px;
@@ -163,7 +170,7 @@ const Postbuttondiv = styled.div`
 function DetailsQustion() {
   const dispatch = useDispatch();
   const forceUpdate = useForceUpdate();
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
   const num = useSelector((state) => state);
   const [Number, SetNumber] = useState(0);
   const [Bookmark, SetBookmark] = useState(true);
@@ -179,19 +186,21 @@ function DetailsQustion() {
     SetBookmark(!Bookmark);
   };
 
-  const [questionsstring, setQuestionsstring] = useState('');
-  const [questionsstring1, setQuestionsstring1] = useState('');
-  const [questionsstring2, setQuestionsstring2] = useState('');
-  const [questionsstring3, setQuestionsstring3] = useState('');
-  const [arrybox, setarrybox] = useState('');
+  const [questionsstring, setQuestionsstring] = useState("");
+  const [questionsstring1, setQuestionsstring1] = useState("");
+  const [questionsstring2, setQuestionsstring2] = useState("");
+  const [questionsstring3, setQuestionsstring3] = useState("");
 
   useEffect(() => {
     axios
       .get(`/questions/${num}`, {})
       .then((response) => {
-        // console.log(response.data.data.questionResponseDto.tagResponseDtos);
         setQuestionsstring(response.data.data.questionResponseDto);
-        console.log('' + response.data.data.questionResponseDto.commentResponseDtos[14].content);
+        console.log(
+          "" +
+            response.data.data.questionResponseDto.commentResponseDtos[14]
+              .content
+        );
       })
       .catch((error) => {
         console.log(error);
@@ -232,17 +241,25 @@ function DetailsQustion() {
 
   const todaynow = new Date();
   const today = (x) => {
-    return todaynow.getFullYear() + (todaynow.getMonth() + 1) + (todaynow.getDate() - x);
+    return (
+      todaynow.getFullYear() +
+      (todaynow.getMonth() + 1) +
+      (todaynow.getDate() - x)
+    );
   };
 
   const createdAtnow = new Date();
-  const createdAtday = createdAtnow.getFullYear() + (createdAtnow.getMonth() + 1) + createdAtnow.getDate() - 3;
+  const createdAtday =
+    createdAtnow.getFullYear() +
+    (createdAtnow.getMonth() + 1) +
+    createdAtnow.getDate() -
+    3;
   // const today = new Date(now.setDate(now.getDate()));
   const RenderingcreatedAt = () => {
     for (let i = 0; i < 31; i++) {
       if (today(i) === createdAtday) {
-        if (i === 0) return 'today';
-        if (i === 1) return 'yesterday';
+        if (i === 0) return "today";
+        if (i === 1) return "yesterday";
         return `${i}days ago`;
       }
     }
@@ -257,7 +274,7 @@ function DetailsQustion() {
       .put(`/questions/15/comments`, {
         memberId: 1,
         commentId: 1,
-        content: 'content',
+        content: "content",
       })
       .then((response) => {
         console.log(response.data.data);
@@ -274,13 +291,17 @@ function DetailsQustion() {
   };
 
   const modifiedAtnow = new Date();
-  const modifiedAtday = modifiedAtnow.getFullYear() + (modifiedAtnow.getMonth() + 1) + modifiedAtnow.getDate() - 2;
+  const modifiedAtday =
+    modifiedAtnow.getFullYear() +
+    (modifiedAtnow.getMonth() + 1) +
+    modifiedAtnow.getDate() -
+    2;
   // const today = new Date(now.setDate(now.getDate()));
   const RenderingmodifiedAt = () => {
     for (let i = 0; i < 31; i++) {
       if (today(i) === modifiedAtday) {
-        if (i === 0) return 'today';
-        if (i === 1) return 'yesterday';
+        if (i === 0) return "today";
+        if (i === 1) return "yesterday";
         return `${i}days ago`;
       }
     }
@@ -296,14 +317,18 @@ function DetailsQustion() {
           {questionsstring.title}
 
           <Importmationdiv>
-            Asked <Importmationspan> {RenderingcreatedAt()}</Importmationspan> Modified
+            Asked <Importmationspan> {RenderingcreatedAt()}</Importmationspan>{" "}
+            Modified
             <Importmationspan>{RenderingmodifiedAt()}</Importmationspan>
-            Viewed{'5'}
+            Viewed{"5"}
             <Importmationspan></Importmationspan>
           </Importmationdiv>
         </Headernamediv>
         <Askbuttondiv>
-          <Link to="/QuestionWrite" class="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary">
+          <Link
+            to="/QuestionWrite"
+            class="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary"
+          >
             Ask Question
           </Link>
         </Askbuttondiv>
@@ -313,7 +338,10 @@ function DetailsQustion() {
           <Recomneddiv>
             <i class="fa-solid fa-caret-up fa-4x" onClick={up}></i>
             {Number}
-            <i class="fa-solid fa-caret-up fa-rotate-180 fa-4x" onClick={Down}></i>
+            <i
+              class="fa-solid fa-caret-up fa-rotate-180 fa-4x"
+              onClick={Down}
+            ></i>
             {Bookmark ? (
               <i class="fa-regular fa-bookmark" onClick={BookmarkHandler}></i>
             ) : (
@@ -324,7 +352,7 @@ function DetailsQustion() {
           </Recomneddiv>
           <Qustioncontentdiv>
             {questionsstring.content}
-            <Codediv>
+            {/* <Codediv>
               <code class="hljs language-python">
                 <span class="hljs-number">a = 1</span>
                 <span class="hljs-number">b = 1</span>
@@ -352,20 +380,20 @@ function DetailsQustion() {
                   f'term: <span class="hljs-subst">{term}</span> / number:{" "}
                   <span class="hljs-subst">{c}</span>'
                 </span>
-                ) a = b b = c */}
+                ) a = b b = c }
               </code>
-            </Codediv>
+            </Codediv> */}
             <Tagboxdiv>
-              <Tagsdiv>react</Tagsdiv>
-              <Tagsdiv>js</Tagsdiv>
-              <Tagsdiv>ptyhon</Tagsdiv>
+              <Tagsdiv>수정된 태그1</Tagsdiv>
+              <Tagsdiv>수정된 태그2</Tagsdiv>
             </Tagboxdiv>
-            <div>Answers</div>
-
-            <div> Your Answer</div>
             <Questioninput onChange={contentHandler} />
             <Postbuttondiv>
-              <button type="submit" class="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary" onClick={handleSubmit}>
+              <button
+                type="submit"
+                class="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary"
+                onClick={handleSubmit}
+              >
                 Post your Answer
               </button>
             </Postbuttondiv>
@@ -376,7 +404,7 @@ function DetailsQustion() {
           <Relateddiv
             onClick={() => {
               forceUpdate();
-              dispatch({ type: 'decrease' });
+              dispatch({ type: "decrease" });
             }}
           >
             <Relatedboxdiv color="#f1f2f3">0</Relatedboxdiv>
